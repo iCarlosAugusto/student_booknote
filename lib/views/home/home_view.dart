04475ed1d9f2e8898c8c205cc88854137918f0b1
Carlos <carlosaugusto@bodyfitteam.com.br>
2023-03-18 20:07:53 -0300

    Criado bottom sheat de criar mateira

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:student_booknote/widgets/appbar_widget.dart';
+import 'package:student_booknote/widgets/button_widget.dart';
 import 'package:student_booknote/widgets/card_subject_widget.dart';
+import 'package:student_booknote/widgets/text_widget.dart';
+import 'package:student_booknote/widgets/textfield_widget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppbarWidget(
         elevation: 0,
         title: 'Minhas matérias',
         actions: [
           IconButton(
-            onPressed: () {}, 
-            icon: const Icon(Icons.filter_1_outlined)
-          )
+              onPressed: () {}, icon: const Icon(Icons.filter_1_outlined))
         ],
       ),
       body: SafeArea(
@@ -27,12 +27,10 @@ class HomeView extends StatelessWidget {
             children: [
               Expanded(
                 child: ListView.separated(
-                  itemBuilder: (_,__) => CardSubjectWidget(
-                    onTap: () => context.pushNamed('/subject')
-                  ),
-                  separatorBuilder: (_,__) => const SizedBox(height: 16),
-                  itemCount: 30
-                ),
+                    itemBuilder: (_, __) => CardSubjectWidget(
+                        onTap: () => context.pushNamed('/subject')),
+                    separatorBuilder: (_, __) => const SizedBox(height: 16),
+                    itemCount: 30),
               )
             ],
           ),
@@ -41,6 +39,47 @@ class HomeView extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () async {
+          showModalBottomSheet<void>(
+            isScrollControlled: true,
+            backgroundColor: Colors.transparent,
+            context: context,
+            builder: (BuildContext context) {
+              return Container(
+                height: MediaQuery.of(context).size.height - 250,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(16),
+                      topRight: Radius.circular(16)),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(top: 16),
+                      width: 100,
+                      height: 5,
+                      decoration: const BoxDecoration(
+                          color: Colors.black,
+                          borderRadius: BorderRadius.horizontal(
+                              left: Radius.circular(10),
+                              right: Radius.circular(10))),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Column(
+                        children: [
+                          const TextWidget('Criar nova matéria'),
+                          const TextfieldWidget(label: 'Nome da matéria', margin: EdgeInsets.only(top: 16, bottom: 16)),
+                          const TextfieldWidget(label: 'Nome do professor', margin: EdgeInsets.only(bottom: 16),),
+                          ButtonWidget(title: 'Criar matéria', onTap: () {}),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          );
         },
       ),
     );
